47e4746be00c85e44760e882abf5d7a796343345
nishchal <144818287+nishchal040@users.noreply.github.com>
2024-08-23 15:41:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { useState } from 'react';
 import Nav from './components/nav';
 import Login from './components/loginpage';
 import Signup from './components/signup';
+import Start from './components/start';
+import Data from './components/data';
+
 function App() {
   
   return (
     <div className="App">
       <Nav/>
+      <Start/>
+      <Data/>
     </div>
   );
 }
