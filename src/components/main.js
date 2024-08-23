c542aafa5ee1b2350d0663bd5593dd81721cb432
Adityakoda <146349549+Adityakoda@users.noreply.github.com>
2024-08-23 15:38:57 +0530

    Add files via upload

---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './main.css';
+import background from './images/pink.jpg';
+function main(){
+    return(
+        <section className="container3">
+            <img src={background} alt="background" />
+        </section>
+    )
+}
+export default main;
